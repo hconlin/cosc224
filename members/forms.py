c7c7b35dab609a9e36cc51eb30721d2c3da82fab
hconlin <hconlinmouat@gmail.com>
2019-02-16 15:23:58 -0800

    added a date selector widget to the form

--- members/forms.py
+++ members/forms.py
@@ -6,10 +6,10 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class SignUpForm(UserCreationForm):
-	email = forms.EmailField(max_length=200, required=True)
-	first_name = forms.CharField(max_length=100, required=True)
-	last_name = forms.CharField(max_length=100, required=True)
-	date_of_birth = forms.DateField()
+	email = forms.EmailField(label='Email', max_length=200, required=True)
+	first_name = forms.CharField(label='First Name', max_length=100, required=True)
+	last_name = forms.CharField(label='Last Name', max_length=100, required=True)
+	date_of_birth = forms.DateField(label='Date of Birth', widget= forms.SelectDateWidget, required=True)
 
 	class Meta:
 		model = User

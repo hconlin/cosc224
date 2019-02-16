16b2db671f1c2d494a944bf3fe62fcf359d1cfc8
hconlin <hconlinmouat@gmail.com>
2019-02-16 15:16:45 -0800

    removed some html

--- createEvent/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CreateeventConfig(AppConfig):
-    name = 'createEvent'
--- createEvent/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-
-class EventForm(forms.Form):
-	Name = forms.CharField()
-	time = forms.DateField()
-	description = forms.CharField()
-	location = forms.CharField()
-

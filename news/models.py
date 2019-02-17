ef7428c51d32b8ac8438ff9a07eefb7f3d96a028
Hayden <hconlinmouat@gmail.com>
2019-02-16 20:54:50 -0800

    added __str__ functions for readability

--- news/models.py
+++ news/models.py
@@ -4,7 +4,9 @@ from members.models import Member
 class News(models.Model):
 	author_id = models.ForeignKey(Member, on_delete=models.CASCADE)
 	title = models.CharField(max_length=200)
-	date = models.DateField('date published')
+	date = models.DateField()
 	body = models.TextField(max_length=2000)
 	image_link = models.CharField(max_length=200)
-	REQUIRED_FIELDS = ['title', 'body']
+
+	def __str__(self):
+		return self.title

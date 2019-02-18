e050019e56bcb328b131d04dfd32e15a3758e813
Hayden <hconlinmouat@gmail.com>
2019-02-17 17:49:46 -0800

    added delete function for admins to events

--- events/urls.py
+++ events/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('create/', views.event_form, name='create'),
-    path('<int:event_id>', views.show, name='show'),
-    path('<int:pk>/edit', views.EditEvent.as_view(), name='edit')
+    path('create/', views.event_form, name='create_event'),
+    path('<int:event_id>', views.show, name='show_event'),
+    path('<int:pk>/edit', views.EditEvent.as_view(), name='edit_event'),
+    path('<int:event_id>/delete', views.deleteEvent, name='delete_event'),
 ]
--- events/views.py
+++ events/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from events.forms import EventForm
+from django.utils.decorators import method_decorator
 from django.contrib.admin.views.decorators import staff_member_required
 from events.models import Event
 from django.views.generic.edit import UpdateView
@@ -25,11 +26,17 @@ def show(request, event_id):
 	event = get_object_or_404(Event, pk=event_id)
 	return render(request, 'events/show.html',{'event': event})
 
-@staff_member_required
+@method_decorator(staff_member_required, name='dispatch')
 class EditEvent(UpdateView):
 	model = Event
 	form_class = EventForm
 	template_name_suffix = '_edit_form'
 
 	def get_success_url(self):
-		return reverse_lazy('show', kwargs={'event_id': self.object.pk})
+		return reverse_lazy('show_event', kwargs={'event_id': self.object.pk})
+
+@staff_member_required
+def deleteEvent(request, event_id):
+    event = get_object_or_404(Event, pk=event_id)
+    event.delete()
+    return HttpResponseRedirect('/')

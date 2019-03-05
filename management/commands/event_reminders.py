daca298107b4eec9a92d6272d35ea9acd0347b19
Andrew <kinnear.andrew@gmail.com>
2019-03-05 14:02:40 -0800

    Event page now has category selection to match with user prefrences, should probably work on the actualy assigned task this sprint which is email validation :P

--- management/commands/event_reminders.py
+++ management/commands/event_reminders.py
@@ -1,13 +1,21 @@
 from django.core.management.base import BaseCommand
 from django.core.mail import send_mail
-from members.models import Member
+from members.models import Member, Preference
 from events.models import Event
 
 class Command(BaseCommand):
 	help = 'Send Event Reminders'
 	
 	def handle(self, *args, **kwargs):
-		
+		all_users = Member.objects.all()
+		#get all events that happen tomorrow
+		#get all users who are interested in event
+		#build message about event and build list of members 
+		tomorrows_date =  datetime.date.today() + datetime.timedelta(days=1)
+		tomorrows_events = Events.objects.filter(start_date__contains=tomorrows_date)
+
+		#for event_tomorrow in tomorrows_events:
+
 
 
 

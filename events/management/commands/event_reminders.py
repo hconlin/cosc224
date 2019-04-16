ee7a06879b12fb1759e29b696ee2dbaa4ba80bf7
Andrew <kinnear.andrew@gmail.com>
2019-04-15 18:49:51 -0700

    Changed email format for reminders

--- events/management/commands/event_reminders.py
+++ events/management/commands/event_reminders.py
@@ -3,6 +3,9 @@ from django.core.mail import send_mail
 from members.models import Member, Preference
 from events.models import Event
 import datetime
+from django.template.loader import render_to_string
+from django.core.mail import EmailMessage
+
 
 class Command(BaseCommand):
 	help = 'Send Event Reminders'
@@ -10,23 +13,33 @@ class Command(BaseCommand):
 	def handle(self, *args, **kwargs):
 
 		tomorrows_date =  datetime.date.today() + datetime.timedelta(days=1)
+		print(tomorrows_date)
 		tomorrows_events = Event.objects.filter(start_date__contains=tomorrows_date)
 		print("working")
-
+		print(tomorrows_events)
 		for event in tomorrows_events:
-			email_list = []
 			prefered_by_users = Preference.objects.filter(preferences__contains=event.category)
 			for user in prefered_by_users:
 				obj = Member.objects.get(pk=user.user_id)
-				email_list.append(obj.email)
-			send_the_mail(email_list, event)
-			print(email_list)
+				print(obj.email)
+				send_the_mail(obj, event)
 
 
+def send_the_mail(user, event):
+	email_address = user.email
+	mail_subject = 'Event Reminder'
+	message = render_to_string('email/reminder.html', {
+		'user': user,
+		'event': event,
+	})
+	email = EmailMessage(
+		mail_subject, message, to=[email_address]
+	)
+	email.send()
 
-def send_the_mail(email_list, event):
-	send_mail('Event Reminder.',
-	event.location_details,
-	'compscieventsOC@gmail.com',
-	email_list,
-	fail_silently = False,)
+
+	# send_mail('Event Reminder.',
+	# event.location_details,
+	# 'compscieventsOC@gmail.com',
+	# email_list,
+	# fail_silently = False,)

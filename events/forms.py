ae5fe755cf804477e2783af37fb9af75a2d03681
Emerald <emerald.mcbride@icloud.com>
2019-04-24 12:06:27 -0700

    added line of text for context

--- events/forms.py
+++ events/forms.py
@@ -28,4 +28,14 @@ class EventForm(forms.ModelForm):
 				  'cost', 'age_requirement', 'link', 'category', 'event_coordinator_name',
 				  'event_coordinator_email', 'image_link']
 
+	def clean(self):
+		cleaned_data = super().clean()
+		start_date = cleaned_data.get("start_date")
+		end_date = cleaned_data.get("end_date")
+		if end_date < start_date:
+			msg = u"End date should be later than start date."
+			self._errors["end_date"] = self.error_class([msg])
+
+
+
 
